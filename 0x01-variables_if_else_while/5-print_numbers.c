29bd4e26cb5f3aa5b4b7996241ff4c2bad183945
NgigiN <ngigi.nyongo@gmail.com>
2022-12-09 11:42:05 -0800

    task_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the code
+ *
+ * return (0) Success
+ */
+
+int main(void)
+{
+	int ch = 0;
+
+	while (ch <= 10)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
