ef9102928d63f9777665fb7352b943d28fd1e22c
NgigiN <ngigi.nyongo@gmail.com>
2022-12-13 10:48:57 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10: prints the alphabet ten times in lowercase
+ *
+ * return: Always (0) Success
+ */
+
+void print_alphabet_x10(void)
+{
+	char t = 'a';
+	int n = 1;
+
+	while (n <= 1)
+	{
+		while (t <= 'z')
+		{
+			_putchar(t);
+			t++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
