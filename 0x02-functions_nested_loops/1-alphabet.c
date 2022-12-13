8f99e81c1666186b1f16f29a14a77e5714b7ffe8
NgigiN <ngigi.nyongo@gmail.com>
2022-12-13 10:35:15 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char t = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(t);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
