26e87957db6c4d544457eb0a68c1eda9d67f933c
NgigiN <ngigi.nyongo@gmail.com>
2022-12-14 07:22:36 -0800

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - entry point
+ * print_alphabet_x10 - entry point
  *
  * Return: 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int n;
 	char c;
