d434d129018869ac7f4ecf6c1738e1b0fbdf5fff
NgigiN <ngigi.nyongo@gmail.com>
2022-12-15 00:02:09 -0800

    +/-

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -tests function thahat prints if integer is positiv or
+ * negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
