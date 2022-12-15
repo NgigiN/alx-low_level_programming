8b7239aefb985632f601718991a6595b4b6aaa6a
NgigiN <ngigi.nyongo@gmail.com>
2022-12-15 12:14:19 -0800

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - gives the product of two numbers
+ * @a: number determined by user
+ * @b: number determined by user
+ * Return: 0 Success
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	printf("%d", c);
+	return (0);
+}
