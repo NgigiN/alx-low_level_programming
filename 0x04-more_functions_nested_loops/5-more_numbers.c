947c654c4624bebd234c63af4dfdf695a7b9520f
NgigiN <ngigi.nyongo@gmail.com>
2022-12-15 12:37:46 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers
+ */
+
+void more_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int c = 0; i < 15; i++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
+}
