ee86b5d91ea7c641293ce8ffc2c4f494d30ddded
NgigiN <ngigi.nyongo@gmail.com>
2022-12-15 14:19:38 -0800

    2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper: checks if a character input is uppercase
- * or lowercase
- * Return: 1 if uppercase and 0 if lowercase
- * @c: is character input by the user
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 if successful
  */
 
 int _isupper(int c)
