fe4a48346ad69b715714f83bad0400cb82701f74
NgigiN <ngigi.nyongo@gmail.com>
2022-12-15 14:06:07 -0800

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,21 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
 			if (i % 3 == 0)
 			{
-				printf("Fizz");
+				printf("Fizz ");
 			}
 			if (i % 5 == 0)
 			{
-				printf("Buzz");
+				printf("Buzz ");
 			}
 			else
 			{
-				printf("%d", i);
+				printf("%d ", i);
 			}
 		}
 	}
