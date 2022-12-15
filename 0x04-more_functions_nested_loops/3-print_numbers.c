fc05a5f63412d8b2f90a22725d012e9c8a62a45e
NgigiN <ngigi.nyongo@gmail.com>
2022-12-15 14:24:01 -0800

    4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers: outputs digits desired
+ * print_numbers - outputs digits desired
  */
 
 void print_numbers(void)
 {
 	for (int c = 0; c < 10; c++)
 	{
-		_putchar(c);
+		putchar(c);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
