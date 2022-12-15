96beaa1822580a40e9622ea94778e231624c291a
NgigiN <ngigi.nyongo@gmail.com>
2022-12-15 12:30:27 -0800

    wow

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ */
+
+void print_most_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+}
