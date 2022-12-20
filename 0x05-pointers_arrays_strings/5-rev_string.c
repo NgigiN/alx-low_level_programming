a8a7ebf2bd0f4ab0eeae20562305e25cd58f92a1
NgigiN <ngigi.nyongo@gmail.com>
2022-12-20 05:55:31 -0800

    r.2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses the string input by the user
+ *@s: the parameter to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
