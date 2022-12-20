fb258d5075ddf2094ee67121c692384a65f62503
NgigiN <ngigi.nyongo@gmail.com>
2022-12-20 05:11:55 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - outputs the string passed to it
+ * @str: the string to be ouput by the function
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
