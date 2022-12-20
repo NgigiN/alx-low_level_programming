fea4af2afd022c0c83edae1d71adc59db224e7f4
NgigiN <ngigi.nyongo@gmail.com>
2022-12-20 04:46:13 -0800

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "stdio.h"
 
 /**
- * reset_to_98: takes a pointer and assigns to it 98
+ * reset_to_98 - takes a pointer and assigns to it 98
+ * @n: parameter passed to the function
  */
 
 void reset_to_98(int *n)
 {
-	n = 98;
+	*n = 98;
 }
