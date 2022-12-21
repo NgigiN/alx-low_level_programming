9ef524eaf85454b73eeb408eac74b46f541f4bda
NgigiN <ngigi.nyongo@gmail.com>
2022-12-21 09:04:45 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: final result of the concatenated strings
+ * @src: parameter to be concatenated
+ * Return: a pointer to the concatenated char string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
