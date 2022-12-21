b4aa0e30272215e663acb145444fc113352f68c9
NgigiN <ngigi.nyongo@gmail.com>
2022-12-21 09:15:07 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,5 +11,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int l = 0, j;
+
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+	for (j = 0; src[j] != '\0'; j++, l++)
+	{
+		dest[l] = src[j];
+	}
+
+	dest[l] = '\0';
+	return (dest);
 }
