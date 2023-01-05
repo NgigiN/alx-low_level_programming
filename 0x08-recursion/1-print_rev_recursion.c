bb6326294de227e856dba083f822f5b2eb5acb05
NgigiN <ngigi.nyongo@gmail.com>
2023-01-05 03:43:11 -0800

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string parameter to be reversed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int g = strlen(s);
+
+	while (g >= 0)
+	{
+		putchar(s[g]);
+		g--;
+}
