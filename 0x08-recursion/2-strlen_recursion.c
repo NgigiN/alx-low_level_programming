7b3d5304003c7bd41451303739dc1d101865647c
NgigiN <ngigi.nyongo@gmail.com>
2023-01-05 09:30:14 -0800

    string_length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string via
+ * recusion
+ * @s: string parameter
+ * Return: in integer of length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	l = strlen(s);
+	return (l);
+}
