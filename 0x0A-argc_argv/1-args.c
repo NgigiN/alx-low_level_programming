93a2f059315f3487ec68215035659ef68ddf9523
NgigiN <ngigi.nyongo@gmail.com>
2023-01-09 11:18:47 -0800

    Arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: counts the arguments
+ * @argv: Arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	print("i\n", argc - 1);
+
+	return (0);
+}
