54552198dd5b16796d37481ff3c8b4363847a62d
NgigiN <ngigi.nyongo@gmail.com>
2023-01-17 06:53:00 -0800

    woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - user defined data type of a dog
+ * @name: name of the dog
+ * @age: how old the dog is
+ * @owner: name of the dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
