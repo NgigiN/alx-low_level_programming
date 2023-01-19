d5715760a47978e76481b8f2ae52188284e31246
NgigiN <ngigi.nyongo@gmail.com>
2023-01-19 10:52:36 -0800

    function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
+#include <stddef.h>
 #define FUNCTION_POINTERS_H
 
 int _putchar(char c);
 void print_name(char *name, void(*F)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
