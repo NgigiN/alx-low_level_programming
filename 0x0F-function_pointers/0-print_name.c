3af0ae015b3d2e31a0311ca84d508361224dcd40
NgigiN <ngigi.nyongo@gmail.com>
2023-01-19 01:53:16 -0800

    pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,119 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}#include <stdio.h>
+
+/**
+ * print_name - prints a name using a pointer to a
+ * function
+ * @name: parameter to be printed
+ * @f: name of the pointer
+ * return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+learning about function pointers and how they work
