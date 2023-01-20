e70cf621e33075c66030dbdfe83e3f39d7786a05
NgigiN <ngigi.nyongo@gmail.com>
2023-01-20 01:33:45 -0800

    ready or not

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include  <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed y a separator
+ * @separator: character to be between the numbers
+ * @n: the numbers of arguments to be passed
+ * return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int l;
+	
+	va_list nums;
+	va_start(nums, n);
+
+	for (l = 0; l < n; l++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (separator != NULL)
+			printf("%s ", separator);
+	}
+	printf("\n");
+	va_end(nums);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
+int main(void);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
