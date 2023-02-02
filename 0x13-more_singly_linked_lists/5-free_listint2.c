b0446ba1440bbbcc385d78a43818c484fa653cd9
NgigiN <ngigi.nyongo@gmail.com>
2023-02-02 14:07:15 +0300

    frees a lists and the head pointer points to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,13 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+        listint_t *temp;
 
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
-	head = NULL;
+        while (head)
+        {
+                temp = (*head)->next;
+                free(*head);
+                *head = temp;
+        }
+        head = NULL;
 }
