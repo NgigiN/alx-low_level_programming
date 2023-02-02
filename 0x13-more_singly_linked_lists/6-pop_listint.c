5a03e844cd33d4f2cdf7a4c074ce02813275ff1c
NgigiN <ngigi.nyongo@gmail.com>
2023-02-02 14:29:34 +0300

    deletes the head noed of a linked list and returns the node data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * and returns the head nodes' data
+ * @head: the pointer to the beginning of the list
+ * Return: head node's data (n) or 0 if the linked list
+ * is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint *temp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = temp;
+
+	return (n);
+}
