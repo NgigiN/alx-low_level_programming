e85b12a8341deeb2dc734c1bad30a5c82a43fdd9
root <root@babyboo>
2023-02-07 15:57:29 +0300

    main.h

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given
+ * index
+ * @n: parameter to be changed
+ * @index: position of bit to be changed
+ * Return: 1 if it works and -1 if it fails
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	
