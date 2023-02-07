4d614c8713283cf7bcb977182c57ef1eda320053
root <root@babyboo>
2023-02-07 07:48:48 +0300

    prints an integer from its binary representation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * print_binary - prints the binary representation
+ * of a number
+ * @n: the integer to be operated
+ * Return: nothing
+ * No use of arrays
+ * No use of malloc
+ * No use of '%' or '/'
+ */
+
+void print_binary(unsigned long int n)
+{
+
