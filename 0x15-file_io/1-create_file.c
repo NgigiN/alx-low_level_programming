10131b342f8766919e4c75b42324e6b3805a0b35
NgigiN <ngigi.nyongo@gmail.com>
2023-02-08 22:58:51 +0300

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (fd == -1)
+	if (fd < o)
 		return (-1);
 
 	while (text_content[len])
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		len++;
 	}
 
-	status = write(fd, text_content, 9);
+	status = write(fd, text_content, len);
 	if (status < 0)
 	{
 	close(fd);
