c68cda167c887421ac1fd35707f6b3800cd12bc1
NgigiN <ngigi.nyongo@gmail.com>
2023-03-16 12:20:44 -0700

    double linked lists in c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count;
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
