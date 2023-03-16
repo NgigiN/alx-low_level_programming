5988230cdddf208d7987f0a1c9f7cd843dd12612
NgigiN <ngigi.nyongo@gmail.com>
2023-03-16 12:46:20 -0700

    Learning about double linked lists in C

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,7 +8,13 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count;
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while(h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
